cf74b530ab384736992ea876ba946f739ee5a613
xuyede <yede.xu@jyblife.com>
2021-01-30 21:59:02 +0800

    feat:栈

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function push(target) {
+  this.dataStore[this.top++] = target;
+  return this.dataStore;
+}
+
+function pop() {
+  return this.dataStore.splice(--this.top, 1);
+}
+
+function peek() {
+  return this.dataStore[this.top - 1];
+}
+
+function clear() {
+  this.dataStore.length = 0;
+  this.top = 0;
+  return this.dataStore;
+}
+
+function length() {
+  return this.top;
+}
+
+
 function Stack() {
   this.dataStore = [];
   this.top = 0;
@@ -6,5 +30,20 @@ function Stack() {
   this.peek = peek; // 返回栈顶元素
   this.clear = clear;
   this.length = length;
-
 }
+
+
+const stack = new Stack();
+
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+
+stack.pop();
+stack.pop();
+
+console.log(stack.dataStore, stack.top, stack.peek())
+// let a = stack.peek();
+
